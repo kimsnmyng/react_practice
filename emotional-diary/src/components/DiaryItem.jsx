b4fcea035fad216511eddf92018ab008c234c58d
keemsnmyn <keemsnmyn@chungbuk.ac.kr>
2024-12-19 16:43:52 +0900

    새로운 일기 쓰기 기능구현

---
+++ emotional-diary/src/components/DiaryItem.jsx
@@ -0,0 +1,32 @@
+import { getEmotionImage } from "../util/get-emotion-image";
+import "./DiaryItem.css";
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
+
+const DiaryItem = ({ id, emotionId, createdDate, content }) => {
+  const nav = useNavigate();
+
+  return (
+    <div className="DiaryItem">
+      <div
+        onClick={() => nav(`/diary/${id}`)}
+        className={`img_section img_section_${emotionId}`}
+      >
+        <img src={getEmotionImage(emotionId)} />
+      </div>
+      <div className="info_section" onClick={() => nav(`/diary/${id}`)}>
+        <div className="create_date">
+          {new Date(createdDate).toLocaleDateString()}
+        </div>
+        <div className="content">{content}</div>
+      </div>
+      <div className="button_section">
+        <Button
+        onClick={() => nav(`/edit/${id}`)}
+        text={"수정하기"} />
+      </div>
+    </div>
+  );
+};
+
+export default DiaryItem;
